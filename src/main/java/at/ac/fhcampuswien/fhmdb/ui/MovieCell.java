739598f8f4302a6d60e9bf9a54c38f01c264f388
illia.tikus <illiatikus@gmail.com>
2023-04-14 20:26:44 +0200

    Improvements for a single movie view

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -14,7 +15,10 @@ public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genre = new Label();
-    private final VBox layout = new VBox(title, detail, genre);
+    private final Label year = new Label();
+    private final Label rating = new Label();
+    private final HBox details = new HBox(genre, year, rating);
+    private final VBox layout = new VBox(title, detail, details);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -32,6 +36,8 @@ public class MovieCell extends ListCell<Movie> {
             );
             String genreWithoutBrackets = movie.getGenres().toString().replaceAll("\\[|\\]", "");
             genre.setText(genreWithoutBrackets);
+            year.setText(movie.getYear());
+            rating.setText(movie.getRating());
 
 
             // color scheme
@@ -39,6 +45,8 @@ public class MovieCell extends ListCell<Movie> {
             detail.getStyleClass().add("text-white");
             genre.getStyleClass().add("text-white");
             genre.getStyleClass().add("font-style: italic");
+            year.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
@@ -47,9 +55,13 @@ public class MovieCell extends ListCell<Movie> {
             detail.setWrapText(true);
             genre.setFont(Font.font("Arial", 12));
             genre.setStyle("-fx-font-style: italic;");
-            layout.setPadding(new Insets(10));
+            year.setFont(Font.font("Arial", 12));
+            rating.setFont(Font.font("Arial", 12));
+
+            layout.setPadding(new Insets(15));
 
             layout.spacingProperty().set(10);
+            details.spacingProperty().set(20);
             layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
             setGraphic(layout);
         }
