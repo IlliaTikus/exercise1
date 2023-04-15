ba11f4fbd3dd2ceca64cc7a1cc4e86aa42387b9d
Alan Matykiewicz <alan.matykiewicz@outlook.com>
2023-04-15 20:38:06 +0200

    extend movie class with remaining fields found in api response

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 public class Movie implements Comparable<Movie>{
 
+    private String id;
     private String title;
     private String description;
     private List<Genre> genres;
-    private String releaseYear;
-    private String rating;
+    private int releaseYear;
+    private double rating;
     private List<String> mainCast;
     private List<String> directors;
+    private String imgUrl;
+    private int lengthInMinutes;
+    private List<String> writers;
 
     public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
@@ -23,10 +27,20 @@ public class Movie implements Comparable<Movie>{
         this.title = title;
         this.description = description;
         this.genres = genres;
-        this.releaseYear = year;
-        this.rating = rating;
+        this.releaseYear = Integer.parseInt(year);
+        this.rating = Double.parseDouble(rating);
         this.mainCast = mainCast;
         this.directors = directors;
+    }
+
+    public Movie(String title, String description, List<Genre> genres, String year, String rating,
+                 List<String> mainCast, List<String> directors, String id, String imgUrl,
+                 String lengthInMinutes, List<String> writers) {
+        this(title, description, genres, year, rating, mainCast, directors);
+        this.id = id;
+        this.imgUrl = imgUrl;
+        this.lengthInMinutes = Integer.parseInt(lengthInMinutes);
+        this.writers = writers;
     }
 
     public String getTitle() {
@@ -41,13 +55,21 @@ public class Movie implements Comparable<Movie>{
         return genres;
     }
 
-    public String getYear() { return releaseYear; }
+    public int getYear() { return releaseYear; }
 
-    public String getRating() { return rating; }
+    public double getRating() { return rating; }
 
     public List<String> getActors() { return mainCast; }
 
     public List<String> getDirectors() { return directors; }
+
+    public String getId() { return id; }
+
+    public String getImgUrl() { return imgUrl; }
+
+    public int getLengthInMinutes() { return lengthInMinutes; }
+
+    public List<String> getWriters() { return writers; }
 
     public boolean containsSubstring(String substring){
         return String.format("%s %s", title, description).toLowerCase().contains(substring.toLowerCase());
