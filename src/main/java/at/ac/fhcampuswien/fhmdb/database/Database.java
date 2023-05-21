7cf36c56f84fc6f1803093919d6c713ccc2258dc
markrei <markus.reiterer@stud.fh-campuswien.ac.at>
2023-05-21 13:59:51 +0200

    add config.properties

--- src/main/java/at/ac/fhcampuswien/fhmdb/database/Database.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/database/Database.java
@@ -5,12 +5,16 @@ import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Database {
-    private static final String DB_URL = "jdbc:h2:file: ./db/watchlist"; // TODO DB URL input
-    private static final String username = "user"; // TODO DB username input
-    private static final String password = "password"; // TODO DB password input
+    private static String DB_URL;
+    private static String username;
+    private static String password;
     private ConnectionSource connectionSource;
     private Dao<WatchlistEntity, Long> dao;
 
@@ -18,16 +22,32 @@ public class Database {
 
     private Database() {
         try {
+            loadConfigurations();
             createConnectionSource();
-            dao = DaoManager.createDao(connectionSource, WatchlistEntity.class);
+            createDao();
             createTables();
-        } catch (SQLException sqlException){
-            System.out.println(sqlException.getMessage());
+        } catch (SQLException | IOException e) {
+            System.out.println(e.getMessage());
         }
+    }
+
+    private void loadConfigurations() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("./config.properties");
+        properties.load(fileInputStream);
+        fileInputStream.close();
+
+        this.DB_URL = properties.getProperty("db.url");
+        this.username = properties.getProperty("db.username");
+        this.password = properties.getProperty("db.password");
     }
 
     public void createConnectionSource() throws SQLException {
         this.connectionSource = new JdbcConnectionSource(this.DB_URL, this.username, this.password);
+    }
+
+    public void createDao() throws SQLException {
+        this.dao = DaoManager.createDao(connectionSource, WatchlistEntity.class);
     }
 
     public ConnectionSource getConnectionSource() {
